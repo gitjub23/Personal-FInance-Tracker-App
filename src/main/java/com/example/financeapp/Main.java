aa5e6e51a0c3153c439a1cdfd310cc6c9beac077
Jucilino <jucilinoaguiar23@gmail.com>
2025-12-08 00:29:02 +0100

    Full JavaFX frontend concluded

--- src/main/java/com/example/financeapp/Main.java
+++ src/main/java/com/example/financeapp/Main.java
@@ -1,4 +1,29 @@
 package com.example.financeapp;
 
-public class Main {
+import com.example.financeapp.navigation.SceneManager;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        // Register the primary stage with the SceneManager
+        SceneManager.setStage(stage);
+
+        // Load the first screen (Login page)
+        SceneManager.switchTo("LoginView");
+
+        stage.setTitle("Finance App");
+
+        stage.setResizable(true);
+        stage.setMinWidth(500);
+        stage.setMinHeight(820);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
